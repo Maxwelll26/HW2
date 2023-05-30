b3673934a52e0bd68e349c712a785297318425b2
Maxwell <maximbudnev@campus.technion.ac.il>
2023-05-30 11:50:03 +0300

    Revert "constant with java docs"
    This reverts commit 68807ec19ce385e96b970e02434374c59d48211c.

--- src/Constant.java
+++ src/Constant.java
@@ -1,38 +1,19 @@
 public class Constant extends Function {
-        private final double constantValue;
+    private final double constant;
 
-    /**
-     * constructor for constant value
-     * @param constantValue double number.
-     */
-    public Constant(double constantValue) {
-            this.constantValue = constantValue;
-        }
-
-    /**
-     * In constant function, no matter what will be the X that we will put in the function,
-     * we will always get the same number - the initial constant number of the function.
-     * @param x - double number that we will "put" in the function
-     * @return the same constant value
-     */
-        @Override
-        public double valueAt(double x) {
-            return constantValue;
-        }
-
-
-        @Override
-        public String toString() {
-            return Double.toString(constantValue);
-        }
-
-    /**
-     * The derivative of a constant function is always 0
-     * @return constant function that represents 0.
-     */
-    @Override
-        public Function derivative() {
-            return new Constant(0);
-        }
+    public Constant(double constant) {
+        this.constant = constant;
     }
-
+    @Override
+    public double valueAt(double x) {
+        return constant;
+    }
+    @Override
+    public String toString() {
+        return "(" + constant + ")";
+    }
+    @Override
+    public Function derivative() {
+        return new Constant(0.0);
+    }
+}
